9ed93b32d343f09e49ced67927704bd8d972e3eb
livstyle <livstylecn@gmail.com>
2023-07-21 15:35:37 +0800

    feat: 适配cicv-classroom

--- spider/src/config/index.ts
+++ spider/src/config/index.ts
@@ -2,9 +2,9 @@ import * as dotenv from "dotenv";
 
 export const config = dotenv.config();
 
-export const organiztion: string = 'learningOS';            // 组织
-export const fullOrganization: string = '19380377-learning-rust'; // classroom名称
-export const assignment = 'rust-rustlings';   // assignment
+export const organiztion: string = 'cicvedu';            // 组织
+export const fullOrganization: string = '119796606-cicv-rust-classroom'; // classroom名称
+export const assignment = 'cicv-rustlings';   // assignment
 export const works = ['main']; // assignment 的不同情况
 export const AUTH_TOKEN = process.env['TOKEN'];
 export const SESSION_TOKEN = process.env['SESSION_TOKEN'];
